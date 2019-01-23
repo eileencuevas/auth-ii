ea62da977a1bc610dab38f959009bc52824ba3a2
steelworx <eileencuevas62@gmail.com>
2019-01-23 17:12:56 -0500

    now allows for user registration using POST endpoint in server.js

--- api/server.js
+++ api/server.js
@@ -2,9 +2,28 @@ const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const middleware = require('./config/middleware');
+const helpers = require('../data/helpers/functionHelpers');
 
 const server = express();
 
 middleware(server);
+
+server.post('/api/register', (req, res) => {
+    const userInfo = req.body;
+    userInfo.password = bcrypt.hashSync(userInfo.password, 16);
+
+    helpers
+        .registerUser(userInfo)
+        .then(id => {
+            res.status(201).json({ 
+                message: `sucessfully registered as user ${id[0]}`
+            });
+        })
+        .catch(() => {
+            res.status(500).json({ 
+                error: `Couldn't register. Please try again with a password and another username.` 
+            })
+        });
+})
 
 module.exports = server;
