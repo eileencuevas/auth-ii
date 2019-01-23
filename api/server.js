19e116189a15a272c7f7c45f86eab10a3db42f0a
steelworx <eileencuevas62@gmail.com>
2019-01-23 17:45:21 -0500

    added POST endpoint for user login

--- api/server.js
+++ api/server.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const middleware = require('./config/middleware');
-const helpers = require('../data/helpers/functionHelpers');
+const helpers = require('../data/helpers/helperFunctions');
 
 const server = express();
 
@@ -22,6 +21,26 @@ server.post('/api/register', (req, res) => {
         .catch(() => {
             res.status(500).json({ 
                 error: `Couldn't register. Please try again with a password and another username.` 
+            })
+        });
+})
+
+server.post('/api/login/', (req, res) => {
+    const creds = req.body;
+
+    helpers
+        .login(creds)
+        .then(user => {
+            if (!user || !bcrypt.compareSync(creds.password, user.password)) {
+                res.status(401).json({ message: 'You shall not pass!' });
+            } else {
+                const token = helpers.generateToken(user);
+                res.status(200).send(token);
+            }
+        })
+        .catch(() => {
+            res.status(500).json({ 
+                error: `Couldn't log in. Please try again.` 
             })
         });
 })
--- data/helpers/helperFunctions.js
+++ data/helpers/helperFunctions.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const knex = require('knex');
 const knexConfig = require('../../knexfile');
+const jwt = require('jsonwebtoken');
 
 const db = knex(knexConfig.development);
 
@@ -7,6 +9,28 @@ const registerUser = userInfo => {
     return db('users').insert(userInfo);
 }
 
+const login = credentials => {
+    return db('users')
+        .where({ 'username': credentials.username })
+        .first();
+}
+
+const generateToken = user => {
+    const payload = {
+        userId: user.id,
+    };
+
+    const secret = process.env.JWT_SECRET;
+
+    const options = {
+        expiresIn: '10m',
+    }
+
+    return jwt.sign(payload, secret, options);
+}
+
 module.exports = {
     registerUser,
+    login,
+    generateToken,
 }
