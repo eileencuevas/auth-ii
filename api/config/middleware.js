dabae61ac6879ebbc08e75273d44e77592014fdb
steelworx <eileencuevas62@gmail.com>
2019-01-23 16:59:12 -0500

    created middleware.js to house middleware

---
+++ api/config/middleware.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+module.exports = server => {
+    server.use(helmet());
+    server.use(morgan('short'));
+    server.use(express.json());
+}
