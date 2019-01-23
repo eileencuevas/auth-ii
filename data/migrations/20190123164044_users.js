988f7c640b1779f2496fa8909976290a9caea511
steelworx <eileencuevas62@gmail.com>
2019-01-23 16:46:57 -0500

    used knex to create database with users table

---
+++ data/migrations/20190123164044_users.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', table => {
+      table.increments();
+      table.string('username', 255)
+        .notNullable()
+        .unique('uq_users_username');
+      table
+        .string('password', 255)
+        .notNullable();
+      table.string('department', 255);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};
